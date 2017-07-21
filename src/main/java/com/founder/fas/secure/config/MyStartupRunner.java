a7a981c04d1bec70f1fbd58cfe631fb9a2c0ae7a
kenlin1 <lichen0503@126.com>
2017-07-21 15:41:38 +0800

    1

--- src/main/java/com/founder/fas/secure/config/MyStartupRunner.java
+++ src/main/java/com/founder/fas/secure/config/MyStartupRunner.java
@@ -5,13 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-/**
- * 服务启动执行
- *
- * @author   单红宇(365384722)
- * @myblog  http://blog.csdn.net/catoop/
- * @create    2016年1月9日
- */
 @Component
 @Order(value=1)
 public class MyStartupRunner implements CommandLineRunner {
